f4888f6d3f4988e1b2b4eb083be81a77c97304a8
Mike Fikes <mike@fikesfarm.com>
2016-12-18 21:59:17 -0500

    Separate out data

---
+++ src/advent_2016/day_01/data.cljs
@@ -0,0 +1,13 @@
+(ns advent-2016.day-01.data)
+
+(def puzzle-input '[R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5,
+                    R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2,
+                    R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190,
+                    R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2,
+                    R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4,
+                    R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1,
+                    R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4,
+                    R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5,
+                    R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1,
+                    R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4,
+                    L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1])
--- src/advent_2016/day_01/part_2.cljs
+++ src/advent_2016/day_01/part_2.cljs
@@ -1,5 +1,6 @@
 (ns advent-2016.day-01.part-2
   (:require
+    [advent-2016.day-01.data :refer [puzzle-input]]
     [clojure.set :as set]
     [clojure.spec :as s]))
 
@@ -147,17 +148,7 @@
 
 (defn solve
   []
-  (let [raw-moves '[R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5,
-                    R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2,
-                    R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190,
-                    R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2,
-                    R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4,
-                    R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1,
-                    R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4,
-                    R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5,
-                    R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1,
-                    R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4,
-                    L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1]
+  (let [raw-moves puzzle-input
         moves (map parse-move raw-moves)
         expanded-moves (mapcat expand-move moves)
         initial-state {::direction ::north
