1a68b2ee81c4b1ab8ac0421764c18f21006eb4ef
Mike Fikes <mike@fikesfarm.com>
2016-12-22 12:54:09 -0500

    Add a transducer solution

--- src/advent_2016/day_5/part_1.cljs
+++ src/advent_2016/day_5/part_1.cljs
@@ -2,10 +2,20 @@
   (:require [advent-2016.day-5.md5 :refer [md5-hex]]
             [clojure.string :as str]))
 
-(defn solve
+(defn solve-sequences
   [s]
   (->> (range)
     (map #(md5-hex (str s %)))
     (filter #(str/starts-with? % "00000"))
     (map #(subs % 5 6))
     (take 8)))
+
+(defn solve-transducers
+  [s]
+  (into []
+    (comp
+      (map #(md5-hex (str s %)))
+      (filter #(str/starts-with? % "00000"))
+      (map #(subs % 5 6))
+      (take 8))
+    (range)))
