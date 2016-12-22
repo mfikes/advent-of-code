a9931a58933a8df4ebe2f4d2e958fe61a6c23c8c
Mike Fikes <mike@fikesfarm.com>
2016-12-22 10:02:08 -0500

    Day 5 md5

---
+++ src/advent_2016/day_5/md5.cljs
@@ -0,0 +1,9 @@
+(ns advent-2016.day-5.md5
+  (:require [goog.crypt :as gcrypt])
+  (:import (goog.crypt Md5)))
+
+(defn md5
+  [s]
+  (-> (doto (Md5.) (.update (gcrypt/stringToUtf8ByteArray s)))
+    .digest
+    gcrypt/byteArrayToHex))
