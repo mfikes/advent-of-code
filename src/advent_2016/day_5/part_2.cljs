b218d7ed34ee03155d7e63643f4b1b8ea45cfece
Mike Fikes <mike@fikesfarm.com>
2016-12-25 15:04:30 -0500

    Day 5 part 2

---
+++ src/advent_2016/day_5/part_2.cljs
@@ -0,0 +1,38 @@
+(ns advent-2016.day-5.part-2
+  (:require [advent-2016.day-5.md5 :refer [md5-hex]]
+            [clojure.string :as str]))
+
+(defn ndx-str
+  [s]
+  (subs s 5 6))
+
+(defn code-str
+  [s]
+  (subs s 6 7))
+
+(defn valid-ndx-str?
+  [s]
+  (#{"0" "1" "2" "3" "4" "5" "6" "7" "8"} s))
+
+(defn candidates
+  [s]
+  (sequence
+    (comp
+      (map #(md5-hex (str s %)))
+      (filter #(str/starts-with? % "00000"))
+      (filter #((comp valid-ndx-str? ndx-str) %)))
+    (range)))
+
+(defn solve
+  [s]
+  (loop [seen #{}
+         candidates (candidates s)
+         result ""]
+    (let [candidate (first candidates)
+          ndx (js/parseInt (ndx-str candidate))]
+      (if (seen ndx)
+        (recur seen (rest candidates) result)
+        (let [result (str result (code-str candidate))]
+          (if (== 8 (count result))
+            result
+            (recur (conj seen ndx) (rest candidates) result)))))))
