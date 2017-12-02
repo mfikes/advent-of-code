e0e6739020eded002c3e2699b24907d7f8664b53
Mike Fikes <mike@fikesfarm.com>
2017-12-02 10:07:39 -0500

    Day 2

---
+++ src/advent_2017/day_02/part_1.cljs
@@ -0,0 +1,18 @@
+(ns advent-2017.day-02.part-1
+  (:refer-clojure :exclude [range])
+  (:require
+   [advent-2017.day-02.data-1 :as data]))
+
+(defn range
+  "Finds the difference between the maximum and minimum values in in the
+   non-empty sequence of numbers xs."
+  [xs]
+  (- (apply max xs) (apply min xs)))
+
+(defn solve
+  ([] (solve data/puzzle-input))
+  ([rows]
+   (transduce
+     (map range)
+     +
+     rows)))
---
+++ src/advent_2017/day_02/part_2.cljs
@@ -0,0 +1,34 @@
+(ns advent-2017.day-02.part-2
+  (:require
+   [advent-2017.day-02.data-2 :as data]))
+
+(defn divides?
+  "Returns true iff x divides y."
+  [x y]
+  (and (not= 0 x y)
+       (zero? (mod y x))))
+
+(defn multiples
+  "Returns a lazy sequence of all distinct pairs elements x1 and x2 such that x1
+  divides x2."
+  [xs]
+  (for [x1 xs
+        x2 xs
+        :when (and (distinct? x1 x2)
+                   (divides? x1 x2))]
+    [x1 x2]))
+
+(defn first-integer-ratio
+  "Returns the ratio of the first pair of distinct elements in xs where one
+  divides the other, or nil if none are found."
+  [xs]
+  (when-let [[x y] (first (multiples xs))]
+    (/ y x)))
+
+(defn solve
+  ([] (solve data/puzzle-input))
+  ([rows]
+   (transduce
+     (map first-integer-ratio)
+     +
+     rows)))
