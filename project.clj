9dcca175281718ae81594d2a61225fdaf86c327c
Mike Fikes <mike@fikesfarm.com>
2017-12-08 15:51:32 -0500

    Update to 1.9.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :profiles {:dev
              {:dependencies [[tubular "1.1.1"]]
               :source-paths ["dev"]}}
-  :dependencies [[org.clojure/clojure "1.9.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/spec.alpha "0.1.143"]
                  [org.clojure/test.check "0.10.0-alpha2"]])
