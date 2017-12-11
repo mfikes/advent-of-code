1b57bb4343e8f5f7fca97365fad955b1a19ca55f
Mike Fikes <mike@fikesfarm.com>
2017-12-11 16:40:29 -0500

    Remove old 2016 solutions

--- src/advent_2016/day_05/md5.cljs
+++
@@ -1,9 +0,0 @@
-(ns advent-2016.day-05.md5
-  (:require [goog.crypt :as gcrypt])
-  (:import (goog.crypt Md5)))
-
-(defn md5-hex
-  [s]
-  (-> (doto (Md5.) (.update (gcrypt/stringToUtf8ByteArray s)))
-    .digest
-    gcrypt/byteArrayToHex))
--- src/advent_2016/day_07/part_1.cljs
+++
@@ -1,1 +0,0 @@
-(ns advent-2016.day-07.part-1)
