224aad1ac6830c80ece04aad875625d6e8a161de
Mike Fikes <mike@fikesfarm.com>
2024-06-09 14:34:38 -0400

    2017 day 2 part 2

--- src/advent_2017/A17D02.m
+++ src/advent_2017/A17D02.m
@@ -16,15 +16,33 @@
     return [rv copy];
 }
 
-- (id)part1
-{    
+- (id)solve:(int (^)(NSArray<NSNumber*>*))block {
     int sum = 0;
     for (NSArray<NSNumber*>* arr in self.data) {
-        NSNumber *maxNumber = [arr valueForKeyPath:@"@max.self"];
-        NSNumber *minNumber = [arr valueForKeyPath:@"@min.self"];
-        sum += maxNumber.intValue - minNumber.intValue;
+        sum += block(arr);
     }
     return @(sum);
+}
+
+- (id)part1 {
+    return [self solve:^int(NSArray<NSNumber*>* arr) {
+        NSNumber *maxNumber = [arr valueForKeyPath:@"@max.self"];
+        NSNumber *minNumber = [arr valueForKeyPath:@"@min.self"];
+        return maxNumber.intValue - minNumber.intValue;
+    }];
+}
+
+- (id)part2 {
+    return [self solve:^int(NSArray<NSNumber*>* arr) {
+        for (NSNumber* x in arr) {
+            for (NSNumber* y in arr) {
+                if (![y isEqualToNumber:x] && !(y.intValue % x.intValue)) {
+                    return y.intValue / x.intValue;
+                }
+            }
+        }
+        return 0;
+    }];
 }
 
 @end
