b51387150a5f55df705220606e9fed48248948da
Mike Fikes <mike@fikesfarm.com>
2024-06-09 15:38:40 -0400

    Day 3, part 1

--- src/advent/main.m
+++ src/advent/main.m
@@ -1,14 +1,18 @@
 #import <Foundation/Foundation.h>
 #import "A17D01.h"
 #import "A17D02.h"
+#import "A17D03.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        A17D01* a17d01 = [[A17D01 alloc] initWithInputPath:@"advent_2017/day_01"];
-        [a17d01 solve];
         
-        A17D02* a17d02 = [[A17D02 alloc] initWithInputPath:@"advent_2017/day_02"];
-        [a17d02 solve];
+        NSArray<ADProblem*>* problems = @[
+            //[[A17D01 alloc] initWithInputPath:@"advent_2017/day_01"],
+            //[[A17D02 alloc] initWithInputPath:@"advent_2017/day_02"],
+            [[A17D03 alloc] initWithInputPath:nil],
+        ];
+        
+        [problems makeObjectsPerformSelector:@selector(solve)];
     }
     return 0;
 }
---
+++ src/advent_2017/A17D03.m
@@ -0,0 +1,147 @@
+#import "A17D03.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Location : NSObject
+
+@property (nonatomic, assign, readonly) int x;
+
+@property (nonatomic, assign, readonly) int y;
+
+@property (nonatomic, readonly) int distance;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+- (instancetype)initWithX:(int)x y:(int)y NS_DESIGNATED_INITIALIZER;
+
+@end
+
+@implementation Location
+
+@dynamic distance;
+
+- (instancetype)initWithX:(int)x y:(int)y
+{
+    if ((self = [super init])) {
+        _x = x;
+        _y = y;
+    }
+    return self;
+}
+
+- (BOOL)isEqual:(id)object
+{
+    if (self == object) return YES;
+    if ([self class] != [object class]) return NO;
+    
+    Location* otherLocation = (Location*)object;
+    
+    return _x == otherLocation.x && _y == otherLocation.y;
+}
+
+- (NSUInteger)hash
+{
+    return 71993 * (_x + 13752) + 933199 * _y;
+}
+
+- (Location*)add:(Location*)other
+{
+    return [[Location alloc] initWithX:_x + other.x y:_y + other.y];
+}
+
+- (NSArray<Location*>*)addLocations:(NSArray<Location*>*)others
+{
+    NSMutableArray<Location*>* rv = [[NSMutableArray alloc] initWithCapacity:others.count];
+    for (Location* other in others) {
+        [rv addObject:[self add:other]];
+    }
+    return [rv copy];
+}
+
+- (int)distance
+{
+    return abs(_x) + abs(_y);
+}
+
+- (NSString*)description
+{
+    return [NSString stringWithFormat:@"[%d %d]", _x, _y];
+}
+
+@end
+
+@implementation A17D03
+
+- (int)data
+{
+    return 368078;
+}
+
+- (NSArray<Location*>*)candidateLocations:(Location*)location
+{
+    return [location addLocations:@[
+        [[Location alloc] initWithX:0 y:-1],
+        [[Location alloc] initWithX:-1 y:0],
+        [[Location alloc] initWithX:0 y:1],
+        [[Location alloc] initWithX:1 y:0]
+    ]];
+}
+
+- (Location*)nthSpiral:(int)n
+{
+    Location* location = [[Location alloc] initWithX:1 y:0];
+    
+    NSMutableSet<Location*>* usedLocations = [[NSMutableSet<Location*> alloc] init];
+    [usedLocations addObject:[[Location alloc] initWithX:0 y:0]];
+    [usedLocations addObject:[[Location alloc] initWithX:1 y:0]];
+    
+    for (int i=0; i<n; i++) {
+        NSArray<Location*>* candidateLocations = [self candidateLocations:location];
+        NSUInteger idx = 0;
+        while (![usedLocations containsObject:candidateLocations[idx]]) {
+            idx++;
+            idx %= candidateLocations.count;
+        }
+        while ([usedLocations containsObject:candidateLocations[idx]]) {
+            idx++;
+            idx %= candidateLocations.count;
+        }
+        location = candidateLocations[idx];
+        [usedLocations addObject:location];
+    }
+    
+    return location;
+}
+
+- (Location*)locationForSquare:(int)square
+{
+    return [self nthSpiral:square - 2];
+}
+
+- (nullable id)part1
+{
+    return @([self locationForSquare:[self data]].distance);
+}
+
+- (NSArray<Location*>*)adjacentLocations:(Location*)location
+{
+    return [location addLocations:@[
+        [[Location alloc] initWithX:-1 y:1],
+        [[Location alloc] initWithX:0 y:1],
+        [[Location alloc] initWithX:1 y:1],
+        [[Location alloc] initWithX:-1 y:0],
+        [[Location alloc] initWithX:1 y:0],
+        [[Location alloc] initWithX:-1 y:-1],
+        [[Location alloc] initWithX:0 y:-1],
+        [[Location alloc] initWithX:1 y:-1]
+    ]];
+}
+
+- (nullable id)part2
+{
+    return nil;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
