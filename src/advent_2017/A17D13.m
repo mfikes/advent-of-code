0e905288c40158c2479ce7d6ca82b0f8a4279437
Mike Fikes <mike@fikesfarm.com>
2024-06-16 20:11:58 -0400

    Day 13

--- src/advent_2017/A17D13.m
+++ src/advent_2017/A17D13.m
@@ -2,16 +2,61 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@implementation A17D13
+@interface A17D13 ()
 
-- (nullable id)part1
-{
-    return nil;
+@property (nonatomic, copy) NSArray<NSArray<NSNumber *> *> *data;
+
+@end
+
+@implementation A17D13 {
+    NSArray<NSArray<NSNumber *> *> *_data;
 }
 
-- (nullable id)part2
-{
-    return nil;
+- (NSArray<NSArray<NSNumber *> *> *)data {
+    if (!_data) {
+        NSMutableArray<NSArray<NSNumber *> *> *tmp = [[NSMutableArray alloc] init];
+        [self.input enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
+            NSArray<NSString *> *components = [line componentsSeparatedByString:@": "];
+            [tmp addObject:@[@(components[0].integerValue), @(components[1].integerValue)]];
+        }];
+        _data = [tmp copy];
+    }
+    return _data;
+}
+
+- (BOOL)isCaughtDepth:(NSInteger)depth range:(NSInteger)range {
+    return !(depth % (2 * (range - 1)));
+}
+
+- (nullable id)part1 {
+    NSInteger sum = 0;
+    for (NSArray<NSNumber *> *arr in self.data) {
+        NSInteger depth = arr[0].integerValue;
+        NSInteger range = arr[1].integerValue;
+        if ([self isCaughtDepth:depth range:range]) {
+            sum += depth * range;
+        }
+    }
+    return @(sum);
+}
+
+- (nullable id)part2 {
+    NSInteger delay = 0;
+    for (;;) {
+        BOOL caught = NO;
+        for (NSArray<NSNumber *> *arr in self.data) {
+            NSInteger depth = arr[0].integerValue;
+            NSInteger range = arr[1].integerValue;
+            if ([self isCaughtDepth:(depth + delay) range:range]) {
+                caught = YES;
+                break;
+            }
+        }
+        if (!caught) {
+            return @(delay);
+        }
+        delay++;
+    }
 }
 
 @end
